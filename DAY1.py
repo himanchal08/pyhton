af419078938541e26851ceee67f497fb8e5edd50
himanchal08 <khattrihimanchal17@gmail.com>
2023-09-18 14:28:21 +0530

    just organising

--- DAY1.py
+++ DAY1.py
@@ -10,27 +10,3 @@ this
 is 
 a multiline comment 
 
-#declaring variables
-a=1
-b=float(3.0948)
-c="john"
-x=3
-y=4
-temp=x
-x=y
-y=temp
-print(x,y , a, b , c )
-
-print("Welcome to the tip calculator .")
-a=float(input("what was the total bill: "))
-b=float(input("How many people to split the bill : "))
-c=float(input("what percentage tip would you like to give? 10 , 12 , 15 : "))
-d=(a*c)/100
-e = a/b
-print("Each person should pay: " , d+e)
-"""
-
-list=['apple' , 'bab' , 'tiger' , 'kio' , 'loki']
-list.remove('tiger')
-list.append('black')
-print(list)
